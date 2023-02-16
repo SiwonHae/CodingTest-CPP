44ff96324b5ed702ccd7cd9ba97d89f0852a5d95
SiwonHae <705lo@naver.com>
2023-02-16 18:20:14 +0900

    BOJ 1629

---
+++ 01/1629.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll A, B, C;
+
+ll go(ll A, ll B) {
+  if (B == 1) {
+    return A % C;
+  }
+
+  ll ret = go (A, B / 2);
+  ret = (ret * ret) % C;
+
+  if (B % 2 == 1) {
+    ret = (ret * A) % C;
+  }
+
+  return ret;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> A >> B >> C;
+  cout << go(A, B);
+
+  return 0;
+};
